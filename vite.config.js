d0e2f4953f09994643f572a8e8d6a18dda082db2
nyxb <contact@nyxb.email>
2023-12-09 22:59:05 +0100

    ✨ feat: add initial project configuration files
    - Add `.vscode/settings.json` for editor configuration
    - Create `package.json` with project metadata and dependencies
    - Include `public/data/sections.json` for defining UI sections
    - Include `public/data/settings.json` for app settings
    - Set up `vite.config.js` for Vite project configuration

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+   base: '/vue-resume-template',
+   plugins: [
+      vue(),
+      VueDevTools(),
+   ],
+})
